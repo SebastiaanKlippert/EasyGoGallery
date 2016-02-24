f6ceec62b8c50da91644c12638eea5648061cc0e
Sebastiaan Klippert <sebastiaan@garagesoftware.nl>
2016-02-25 00:02:52 +0100

    Revert "first very basic gallery"
    This reverts commit 7112928bbd7caf7f2d41a4c98546df51b2a99661.

--- README.md
+++ README.md
@@ -1,9 +1,4 @@
 [![Build Status](https://travis-ci.org/SebastiaanKlippert/EasyGoGallery.svg?branch=master)](https://travis-ci.org/SebastiaanKlippert/EasyGoGallery)
 
 # EasyGoGallery
-
-## What?
-A simple web photo gallery in go (work in progress)
-
-## Why?
-I want to replace an ancient PHP album that runs slow on my Raspberry Pi 1 with something faster...
+a simple web photo gallery in go
--- main.go
+++ main.go
@@ -1,32 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
-
 func main() {
-	err := http.ListenAndServe(":6543", GalleryHandler{})
-	if err != nil {
-		log.Fatal(err)
-	}
-}
 
-type GalleryHandler struct{}
-
-func (h GalleryHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "", "/":
-		index(w, r)
-	case "/thumb":
-		serveFile(w, r)
-	default:
-		serveGallery(w, r)
-	}
-
-}
-
-func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Welcome")
 }
