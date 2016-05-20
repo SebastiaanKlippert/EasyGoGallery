d57b3eba9d11c0aca9d1622673d53e1d08feb716
Sebastiaan Klippert <sebastiaan@garagesoftware.nl>
2016-05-20 22:33:10 +0200

    provide logging to file and log option for failed auth

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"runtime"
 )
 
 var cfg *Config
@@ -16,6 +18,22 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	if cfg.LogFile != "" {
+		f, err := os.OpenFile(cfg.LogFile, os.O_RDWR|os.O_APPEND, os.ModeAppend)
+		if err != nil {
+			f, err = os.Create(cfg.LogFile)
+		}
+		if err != nil {
+			log.Println(err)
+		}
+		if f != nil {
+			log.Printf("Output set to %s", cfg.LogFile)
+			log.SetOutput(f)
+		}
+	}
+
+	log.Printf("Using %d of %d CPUs", runtime.GOMAXPROCS(0), runtime.NumCPU())
 
 	if cfg.CertFile == "" {
 		log.Println("Starting HTTP server on ", cfg.ListenAddr)
@@ -56,10 +74,12 @@ func (h GalleryHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 type Config struct {
 	ListenAddr    string
 	BaseURL       string
+	LogFile       string
 	CertFile      string
 	KeyFile       string
 	BasicAuthUser string
 	BasicAuthPass string
+	LogAuth       bool
 }
 
 func readConfig() error {
@@ -78,9 +98,25 @@ func readConfig() error {
 func checkBasicAuth(w http.ResponseWriter, r *http.Request) bool {
 	u, p, ok := r.BasicAuth()
 	if ok && u == cfg.BasicAuthUser && p == cfg.BasicAuthPass {
+		//logAuth(r, true, &u, &p)
 		return true
 	}
 	w.Header().Add("Www-Authenticate", "Basic")
 	w.WriteHeader(http.StatusUnauthorized)
+	logAuth(r, false, &u, &p)
 	return false
+}
+
+func logAuth(r *http.Request, allowed bool, u, p *string) {
+	if !cfg.LogAuth {
+		return
+	}
+	ok, creds := "DENIED", ""
+	if allowed {
+		ok = "ALLOWED"
+	} else {
+		creds = fmt.Sprintf(" Used credentials (user - pass): %s - %s", *u, *p)
+	}
+
+	log.Printf("[AUTH] Access %s for %s on %s%s\n", ok, r.RemoteAddr, r.URL.Path, creds)
 }
